e7681f99deecd6989ceb78c926d9da79a4c0e31a
Lakshmi Narayana Reddy Dwarampudi <lakshmi.n.dwarampudi@gmail.com>
2022-04-05 11:58:23 +0530

    updated comment-1

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "THis script is from github"
 NUMBER=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo $NUMBER
 echo "its completed"
-echo "it's triggered automatically"
+echo "it's triggered automatically1"
